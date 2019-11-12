5ed5523883748c3e476b6290339603216bf9dd02
fuse999 <rileypence4@gmail.com>
2019-11-12 15:10:57 -0800

    added sql database

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from .models import DB
 
-# now I made a app factory
+# now we make a app factory
 
 def create_app():
     app = Flask(__name__)
 
-    # Add config
-    app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///df.sqlite3"
+    # add config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
-    # Have the database know about the app
+    # now have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -1,16 +1,17 @@
-"""Database models"""
+"""These are my database models"""
 
 from flask_sqlalchemy import SQLAlchemy
 
-# import database
+# import database, capital for global scope
 DB = SQLAlchemy()
 
 class User(DB.Model):
-    """Twitter users to analise"""
+    """Twitter users that we analyze"""
     id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
 
+
 class Tweet(DB.Model):
-    """The users tweets from twitter"""
+    """The user's tweets from twitter"""
     id = DB.Column(DB.Integer, primary_key=True)
     text = DB.Column(DB.Unicode(280))
