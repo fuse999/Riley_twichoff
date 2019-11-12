e92a7aadef414f11bd1218261ffaf8063c646577
fuse999 <rileypence4@gmail.com>
2019-11-11 19:04:19 -0800

    Added templates

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 """Minmal flask app"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 # Make App
 app = Flask(__name__)
@@ -10,4 +10,10 @@ app = Flask(__name__)
 
 # Making the first function
 def hello():
-    return "Hello World"
+    return render_template('home.html')
+
+# Creating another route
+@app.route("/about")
+
+def info():
+    return render_template('about.html')
