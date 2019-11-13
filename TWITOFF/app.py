f49789faa6ff1c912fc47971d48b9ff98e7c2b4a
Riley Pence <48462386+fuse999@users.noreply.github.com>
2019-11-12 16:52:45 -0800

    Add files via upload

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,18 +1,28 @@
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 # now we make a app factory
+
 
 def create_app():
     app = Flask(__name__)
 
-    # add config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    # add our config
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # now have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset', users=[])
     return app
